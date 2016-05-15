2aa58b4e1b7878698d46672f63a084b7e7622be5
kengleng <keng-leng.ng@autodesk.com>
2016-05-15 23:12:30 +0800

    serve index.html

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 
 var app = express();
+
+app.use('/', express.static(__dirname + '/../client/www'));
+
+
 var port = process.env.PORT || 4321;
 app.listen(port);
 console.log("listeniing to port:" + port);
